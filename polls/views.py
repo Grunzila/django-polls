76458f0aa5e55af049f72a00a43e12f93aa2a813
Paul Roylance <grunzila@gmail.com>
2017-04-18 23:10:44 +0000

    created view with template

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,15 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
